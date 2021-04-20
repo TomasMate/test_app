8982fa1ca3479cfbf5c3c98d46eb82c5bd5361b6
TomasMate <82169863+TomasMate@users.noreply.github.com>
2021-04-20 14:51:58 +0200

    Created pages controller with 'Rails generate controller pages'

--- db/migrate/20210420112044_create_articles.rb
+++ db/migrate/20210420112044_create_articles.rb
@@ -2,9 +2,8 @@ class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :string
       t.string :description
-      t.string :string
+    
 
       t.timestamps
     end
